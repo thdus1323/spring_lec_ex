5958a98f95d9e6b2a61a3e6b088a89e6302adc94
thdus <thdus1323@naver.com>
2024-02-20 11:46:54 +0900

    예외처리하는 법

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import shop.mtcoding.blog._util.Script;
 
 
 @RequiredArgsConstructor // final이 붙은 애들에 대한 생성자를 만들어줌
@@ -19,32 +21,29 @@ public class UserController {
     // 왜 조회인데 post임? 민간함 정보는 body로 보낸다.
     // 로그인만 예외로 select인데 post 사용
     // select * from user_tb where username=? and password=?
-    @PostMapping("/login")
-    public String login(UserRequest.LoginDTO requestDTO) {
+    public User findByUsernameAndPassword(UserRequest.LoginDTO requestDTO) {
+        Query query = em.createNativeQuery("select * from user_tb where username=? and password=?", User.class);
+        query.setParameter(1, requestDTO.getUsername());
+        query.setParameter(2, requestDTO.getPassword());
 
-
-        System.out.println(requestDTO); // toString -> @Data
-
-        if (requestDTO.getUsername().length() < 3) {
-            return "error/400"; // ViewResolver 설정이 되어 있음. (앞 경로, 뒤 경로)
+        try {
+            User user = (User) query.getSingleResult();
+            return user;
+        } catch (Exception e) {
+            throw new RuntimeException("아이디 혹은 비밀번호를 찾을 수 없습니다");
         }
-
-        User user = userRepository.findByUsernameAndPassword(requestDTO);
-
-        if (user == null) { // 조회 안됨 (401)
-            return "error/401";
-        } else { // 조회 됐음 (인증됨)
-            session.setAttribute("sessionUser", user); // 락카에 담음 (StateFul)
-        }
-
-        return "redirect:/"; // 컨트롤러가 존재하면 무조건 redirect 외우기
+    }
     }
 
     @PostMapping("/join")
     public String join(UserRequest.JoinDTO requestDTO) {
         System.out.println(requestDTO);
 
-        userRepository.save(requestDTO); // 모델에 위임하기
+        try {
+            userRepository.save(requestDTO); // 모델에 위임하기
+        } catch (Exception e) {
+           throw new RuntimeException("아이디가 중복되었어요");
+        }
         return "redirect:/loginForm";
     }
 
